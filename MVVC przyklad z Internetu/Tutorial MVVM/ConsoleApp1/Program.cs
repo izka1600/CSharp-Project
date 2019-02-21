30fdf087096a771e605a3156df5ad6277b92fb4c
Iza <izakukawska1600@gmail.com>
2019-02-21 22:58:41 +0100

    Tutorial MVVM
    https://www.youtube.com/watch?v=z5t90kvagTw

---
+++ MVVC przyklad z Internetu/Tutorial MVVM/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Customer
+    {
+        private string _CustomerName;
+        public string CustomerName { get; set; }
+
+        private double _Amount;
+        public double Amount { get; set; }
+
+        private string _Married;
+        public string Married { get; set; }
+    }
+}
