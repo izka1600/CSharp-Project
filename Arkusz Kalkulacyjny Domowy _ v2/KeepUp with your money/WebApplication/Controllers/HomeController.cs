0b63afea08a49d0b55085d6d9dcedda3d58354e2
Iza <izakukawska1600@gmail.com>
2019-08-22 18:12:46 +0200

    First steps in Layouts

--- Arkusz Kalkulacyjny Domowy _ v2/KeepUp with your money/WebApplication/Controllers/HomeController.cs
+++ Arkusz Kalkulacyjny Domowy _ v2/KeepUp with your money/WebApplication/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace WebApplication.Controllers
 			return View();
 		}
 
+		public IActionResult TEst()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
---
+++ Arkusz Kalkulacyjny Domowy _ v2/KeepUp with your money/WebApplication/Models/UserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+	public class UserViewModel
+	{
+		[Display(Name = "Id Użytkownika")]
+		[Required]
+		public int ReaderId { get; set; }
+
+		[Display(Name = "Imię Użytkownika")]
+		[Required]
+		[StringLength(30, ErrorMessage = "Imię Użytkownika jest za długie")]
+		public string FirstName { get; set; }
+
+		[Display(Name = "Nazwisko Użytkownika")]
+		[Required]
+		[StringLength(30, ErrorMessage = "Nazwisko Użytkownika jest za długie")]
+		public string LastName { get; set; }
+
+		[Display(Name = "Email Użytkownika")]
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Display(Name = "Prawa dostępu")]
+		[Required]
+		public string AccessRights { get; set; }
+	}
+}
