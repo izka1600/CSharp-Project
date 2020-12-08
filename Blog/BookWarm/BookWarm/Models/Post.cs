ebb48fdfb1b57a70050abf318a544d741baa8873
Iza Kukawska <izakukawska1600@gmail.com>
2020-12-08 23:19:20 +0100

    BookWarm Blog - first scratch

---
+++ Blog/BookWarm/BookWarm/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookWarm.Models
+{
+	public class Post
+    {
+		public string Title { get; set; } = "";
+		public string Body { get; set; } = "";
+		public DateTime Created { get; set; } = DateTime.Now;
+
+	}
+}
