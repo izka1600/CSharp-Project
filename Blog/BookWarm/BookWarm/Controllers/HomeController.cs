1ddcb40817910789130e0a869c833124abd43410
Iza Kukawska <izakukawska1600@gmail.com>
2021-01-10 13:54:52 +0100

    Publish to real web server

--- Blog/BookWarm/BookWarm/Controllers/HomeController.cs
+++ Blog/BookWarm/BookWarm/Controllers/HomeController.cs
@@ -21,24 +21,40 @@ namespace BookWarm.Controllers
             _repo = repo;
             _fileManager = fileManager;
 		}
-        public IActionResult Index(string category)
-		{
-            var posts = string.IsNullOrEmpty(category) ? _repo.GetAllPosts() : _repo.GetAllPosts(category);
-            return View(posts); 
-		}
+        //public IActionResult Index(string category)
+		//{
+        //   var posts = string.IsNullOrEmpty(category) ? _repo.GetAllPosts() : _repo.GetAllPosts(category);
+        //   return View(posts); 
+		//}
 
-        public IActionResult Post(int id)
-        {
-            var post = _repo.GetPost(id);
-            return View(post);
-        }
+        public IActionResult Index(string category) =>
+            View(string.IsNullOrEmpty(category) ? 
+                _repo.GetAllPosts() : 
+                _repo.GetAllPosts(category));
+
+        //public IActionResult Post(int id)
+        //{
+        //    var post = _repo.GetPost(id);
+        //    return View(post);
+        //}
+
+        public IActionResult Post(int id) =>
+            View(_repo.GetPost(id));
+
+        //[HttpGet("/Image/{image}")]
+        //public IActionResult Image(string image)
+        //{
+        //    var mime = image.Substring(image.LastIndexOf('.') + 1); //without dot
+        //    return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
+        //}
 
         [HttpGet("/Image/{image}")]
-        public IActionResult Image(string image)
-		{
-            var mime = image.Substring(image.LastIndexOf('.')+1); //without dot
-            return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
-		}
+        public IActionResult Image(string image) =>
+            new FileStreamResult(
+                _fileManager.ImageStream(image), 
+                $"image/{image.Substring(image.LastIndexOf('.') + 1)}"
+                );
+		
 
     }
 }
