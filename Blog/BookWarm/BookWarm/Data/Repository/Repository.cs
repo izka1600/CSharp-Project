3873e39550c8eb0726e8780d58b2fa7f942d8272
Iza Kukawska <izakukawska1600@gmail.com>
2021-01-19 22:13:14 +0100

    User Signing In

--- Blog/BookWarm/BookWarm/Data/Repository/Repository.cs
+++ Blog/BookWarm/BookWarm/Data/Repository/Repository.cs
@@ -1,4 +1,5 @@
-﻿using BookWarm.Models;
+﻿using BookWarm.Helpers;
+using BookWarm.Models;
 using BookWarm.Models.Comments;
 using BookWarm.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -51,54 +52,10 @@ namespace BookWarm.Data.Repository
 				PageNumber = pageNumber,
 				PageCount = (int)Math.Ceiling((double)postsCount / pageSize),
 				NextPage = postsCount > capacity,
-				Pages = PageNumbers(pageNumber, (int)Math.Ceiling((double)postsCount / pageSize)),
+				Pages = PageHelper.PageNumbers(pageNumber, (int)Math.Ceiling((double)postsCount / pageSize)).ToList(),
 				Category = category
 			};
 		} 
-
-		private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
-		{
-			int midPoint = pageNumber;
-			if (midPoint < 3)
-			{
-				midPoint = 3;
-			}
-			else if (midPoint > pageCount - 2)
-			{
-				midPoint = pageCount - 2 > 3 ? pageCount - 2 : 3;
-			}
-
-			int lowerBound = midPoint - 2;
-			int upperBound = midPoint + 2;
-
-			if (lowerBound != 1)
-			{
-				// pages.Insert(0, 1);
-				yield return 1;
-				if (lowerBound-1 > 1)
-				{
-					//pages.Insert(1, -1);
-					yield return -1;
-				}
-			}
-
-			for (int i = lowerBound; i <= upperBound; i++)
-			{
-				yield return i;
-				//pages.Add(i);
-			}
-
-			if (upperBound < pageCount)
-			{
-				//pages.Insert(pages.Count, pageCount);
-				if (pageCount-upperBound > 1)
-				{
-					//pages.Insert(pages.Count - 1, -1);
-					yield return -1;
-				}
-				yield return pageCount;
-			}
-		}
 
 		public Post GetPost(int id)
 		{
---
+++ Blog/BookWarm/BookWarm/Helpers/PageHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWarm.Helpers
+{
+	public static class PageHelper
+	{
+		public static IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
+		{
+			if (pageCount <= 5)
+			{
+				for (int i = 1; i <= pageCount; i++)
+				{
+					yield return i;
+					//pages.Add(i);
+				}
+			}
+			else
+			{
+
+				int midPoint = pageNumber;
+				if (midPoint < 3)
+				{
+					midPoint = 3;
+				}
+				else if (midPoint > pageCount - 2)
+				{
+					midPoint = pageCount - 2 > 3 ? pageCount - 2 : 3;
+				}
+
+				int lowerBound = midPoint - 2;
+				int upperBound = midPoint + 2;
+
+				if (lowerBound != 1)
+				{
+					// pages.Insert(0, 1);
+					yield return 1;
+					if (lowerBound - 1 > 1)
+					{
+						//pages.Insert(1, -1);
+						yield return -1;
+					}
+				}
+
+				for (int i = lowerBound; i <= upperBound; i++)
+				{
+					yield return i;
+					//pages.Add(i);
+				}
+
+				if (upperBound < pageCount)
+				{
+					//pages.Insert(pages.Count, pageCount);
+					if (pageCount - upperBound > 1)
+					{
+						//pages.Insert(pages.Count - 1, -1);
+						yield return -1;
+					}
+					yield return pageCount;
+				}
+			}
+		}
+	}
+}
