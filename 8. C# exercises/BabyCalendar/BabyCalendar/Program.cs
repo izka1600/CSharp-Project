ee18b4c5328a1810cc53ca75e6cd8246d537340e
Iza Kukawska <izakukawska1600@gmail.com>
2021-11-29 17:58:36 +0100

    Baby week calendar

---
+++ 8. C# exercises/BabyCalendar/BabyCalendar/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace BabyCalendar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime dUrodzenia = Convert.ToDateTime("1900-01-01");
+			while (dUrodzenia == Convert.ToDateTime("1900-01-01"))
+			{
+				dUrodzenia = new WorkFlows().AskUser();
+			};
+
+			//Jaka jest różnica w tygodniach między dniem urodzenia a dzisiaj?
+			Console.WriteLine("Dzisiaj jest " + DateTime.Now );
+			Console.WriteLine("Różnica między dniem urodzenia a dzisiaj to " + DateAndTime.DateDiff(DateInterval.WeekOfYear,dUrodzenia,DateTime.Now) + " tygodni.");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 8. C# exercises/BabyCalendar/BabyCalendar/WorkFlows.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabyCalendar
+{
+	public class WorkFlows :IWorkFlows
+	{
+		public DateTime AskUser()
+		{
+			Console.WriteLine("Podaj datę urodzenia dziecka");
+			string dataUrodzenia = Console.ReadLine();
+			DateTime dUrodzenia = Convert.ToDateTime("1900-01-01");
+
+			try
+			{
+				dUrodzenia = Convert.ToDateTime(dataUrodzenia);
+			}
+			catch (Exception)
+			{
+
+				Console.WriteLine("Niepoprawny format daty");
+			}
+			return dUrodzenia;
+		}
+	}
+}
